207e0c1d5029d0b4bcefc4e3e65f73d09f7b0c25
aski02 <niklas_id@icloud.com>
2025-11-01 11:15:43 +0100

    chore: add hot reloading

--- code/backend/main.py
+++ code/backend/main.py
@@ -1,7 +1,14 @@
 """Application entry point"""
 
+import os
 import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    reload = os.getenv("RELOAD", "false").lower() == "true"
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=reload
+    )
